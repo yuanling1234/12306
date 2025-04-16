32eff11467f46f4271d32f18db77123c570a08a4
yuanling <647825492@qq.com>
2025-04-16 11:06:57 +0800

    upload project

---
+++ console-vue/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          'border-radius-base': '4px',
+          'card-radius': '4px'
+        }
+      }
+    }
+  }
+}
